3b6ea6da0e9ae92e195c190f4e92575403520dac
Jeannie Yamakawa <jkyamakawa@gmail.com>
2016-10-12 13:10:33 -0500

    removes cookie-parser

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,6 @@ const express = require('express');
 const router = express.Router();
 const knex = require('../db/knex')
 const bcrypt = require('bcrypt');
-const cookieParser = require('cookie-parser');
 const methodOverride = require('method-override')
 
 
