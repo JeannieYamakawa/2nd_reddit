7e92b91fd7a94ad51cb13efab267ca067dc270d4
Jeannie Yamakawa <jkyamakawa@gmail.com>
2016-10-12 13:28:55 -0500

    fixes production info in knexfile

--- knexfile.js
+++ knexfile.js
@@ -10,7 +10,20 @@ module.exports = {
     },
 production: {
     client: 'pg',
-    connection:   'postgres://pqysxyyasdboqa:gyUmXJEk-a0tT3wegYj5YEhOB2@ec2-23-23-225-246.compute-1.amazonaws.com:5432/d5p2ugapnpc6ch'
+    connection:   {
+        dbname: 'd5p2ugapnpc6ch',
+        host: 'ec2-23-23-225-246.compute-1.amazonaws.com',
+        user: 'pqysxyyasdboqa',
+        password: 'gyUmXJEk-a0tT3wegYj5YEhOB2',
+        sslmode: 'require'
+    }
+
+
+    "dbname=d5p2ugapnpc6ch host=ec2-23-23-225-246.compute-1.amazonaws.com port=5432 user=pqysxyyasdboqa password=gyUmXJEk-a0tT3wegYj5YEhOB2 sslmode=require"
+
+    'postgres://pqysxyyasdboqa:gyUmXJEk-a0tT3wegYj5YEhOB2@ec2-23-23-225-246.compute-1.amazonaws.com:5432/d5p2ugapnpc6ch'
+
+
 },
 
   development: {
