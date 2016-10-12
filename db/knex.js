2d5a555a5c0787d0c834810cf762a07344ab43e8
Jeannie Yamakawa <jkyamakawa@gmail.com>
2016-10-12 13:54:55 -0500

    connection error remedy

--- db/knex.js
+++ db/knex.js
@@ -1,3 +1,13 @@
-var environment = 'development';
-var config = require('../knexfile.js')[environment];
-module.exports = require('knex')(config);
+//
+// var environment = 'development';
+// var config = require('../knexfile.js')[environment];
+// module.exports = require('knex')(config);
+
+
+const env = process.env.NODE_ENV || 'development';
+const config = require('../knexfile.js')[env];
+const knex = require('knex')(config);
+
+console.log('the environment is ' + env);
+
+module.exports = knex;
--- knexfile.js
+++ knexfile.js
@@ -8,17 +8,7 @@ module.exports = {
         database: '2nd_reddit_test'
       }
     },
-production: {
-    client: 'pg',
-    connection:   {
-        dbname: 'd5p2ugapnpc6ch',
-        host: 'ec2-23-23-225-246.compute-1.amazonaws.com',
-        user: 'pqysxyyasdboqa',
-        password: 'gyUmXJEk-a0tT3wegYj5YEhOB2',
-        sslmode: 'require'
-    }
 
-},
 
   development: {
     client: 'pg',
@@ -27,5 +17,9 @@ production: {
       database:  '2nd_reddit_dev'
     }
   },
+  production: {
+      client: 'pg',
+      connection: 'postgres://pqysxyyasdboqa:gyUmXJEk-a0tT3wegYj5YEhOB2@ec2-23-23-225-246.compute-1.amazonaws.com:5432/d5p2ugapnpc6ch',
 
+  }
 };
